e17c9c49578e66853223927b5783f7127b95eb2a
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-01-20 21:10:14 +0530

    optimized approach

--- Questions/palindrome.cpp
+++ Questions/palindrome.cpp
@@ -1,18 +1,36 @@
-#include<iostream>
-using namespace std;
+    #include<iostream>
+    using namespace std;
 
-void isPlaindrome(string in){
-    int len=in.length();
-    for (int i = 0; i < len; i++)
-    {
+    //optmized approach
+    //we are using optimized approach in which we are checking half digit only.
+    bool palindrome(int x){
+        if(x<0 || x%10==0 || x!=0){
+            return false;
+        }
+
+        int reverseHalf=0;
+
+        while (x>reverseHalf)
+        {
+            reverseHalf=(reverseHalf*10)+(x%10);
+            x=x/10;
+        }
+
+        if(reverseHalf==x ){return true;}
+        else{return false;}
         
     }
-    
-}
 
-main(){
-string in;
-cin>>in;
+    main(){
+    int in;
+    cin>>in;
+   bool check= palindrome(in);
+  if (palindrome(in))
+  {
+    cout<<"true";
+  }
+  else{cout<<"false";}
+  
 
-return 0;
-}
+    return 0;
+    }
