556e26030b6bd782a624d31ae3f8e45ef81a890c
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-02-03 11:49:34 +0530

    rat-maze-half-solved

--- Algorithms/Backtracking/rat-maze-with-multiple-jumps.cpp
+++ Algorithms/Backtracking/rat-maze-with-multiple-jumps.cpp
@@ -13,3 +13,48 @@ ELSE
 
 
 */
+
+#include<iostream>
+using namespace std;
+main(){
+// int n;
+// cin>>n;
+
+int arr[4][4]={9,0,0,0,2,6,0,0,0,7,0,0,0,5,4,3};
+int rpos=arr[0][0];
+arr[0][0]=1;
+for (int i = 0; i < 4; i++)
+{
+    for(int j=0;j<4;j++){
+    
+    
+    if (arr[i][j+1]>0)
+    {
+        rpos=arr[i][j+1];
+        arr[i][j+1]=1;
+    }
+    else if(arr[i+1][j] > 0){
+        rpos=arr[i+1][j];
+        arr[i+1][j]=1;
+        }
+    
+    else if(arr[i][j+1]<=0 && arr[i+1][j] <= 0){
+        // rpos=
+    }
+    }
+}
+
+for (int i = 0; i < 4; i++)
+{
+    for (int k = 0; k < 4; k++)
+    {
+        cout<<arr[i][k]<<" ";
+    }
+    cout<<endl;
+    
+}
+
+
+
+return 0;
+}
