bdc1bf93d897685408aa48832e15e4380ba62d8d
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-03-04 02:23:45 +0530

    moved the files to the parent folder

--- sum_of_row_column.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-main(){
-
-
-
-return 0;
-}
--- test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream> 
-using namespace std; 
-  
-int main() 
-{ 
-    int n=13;
-    cout<<n/4;
-}
